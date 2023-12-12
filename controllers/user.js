ac22738ae9601362ec0fa6a443a6e8866c3fd071
zakinugroho17 <zakibeatbox@gmail.com>
2023-12-12 15:10:23 +0700

    feat login register

--- controllers/user.js
+++ controllers/user.js
@@ -1,56 +1,93 @@
-const { UserProfile } = require('../models/index')
+const { comparePassword } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jwt");
+const { UserProfile } = require("../models/index");
+const { User } = require("../models/index");
 
-class User {
+class controllerUser {
   static async register(req, res, next) {
     try {
+      let data = await User.create(req.body);
+      res.status(201).json({ username: data.username, email: data.email });
       res.send({
-        message: "Register"
-      })
+        message: "Register",
+      });
     } catch (error) {
-      next(error)
+      console.log(error);
+      next(error);
     }
   }
 
   static async login(req, res, next) {
     try {
+      let data = await User.findOne({
+        where: {
+          email: req.body.emailOrUsername,
+        },
+      });
+      if (!data) {
+        let user = await User.findOne({
+          where: {
+            username: req.body.emailOrUsername,
+          },
+        });
+        if (user) {
+          throw { name: "invalidUser/Password" };
+        }
+      }
+
+      const isValid = comparePassword(req.body.password, data.password)
+      if(isValid === true){
+        const token = createToken({
+          id : data.id
+        })
+        res.status(200).json({ access_token : token, email : data.email, username : data.username})
+      } else {
+        throw { name: "invalidUser/Password" };
+      }
+
+
       res.send({
-        message: "Login"
-      })
+        message: "Login",
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 
   static async update(req, res, next) {
     try {
-      const { name, image } = req.body
-      const { id } = req.user
+      const { name, image } = req.body;
+      const { id } = req.user;
 
-      await UserProfile.update({
-        name, image
-      }, {
-        where: {
-          userId: id
+      await UserProfile.update(
+        {
+          name,
+          image,
+        },
+        {
+          where: {
+            userId: id,
+          },
         }
-      })
-      
+      );
+
       const profile = await UserProfile.findOne({
         where: {
-          userId: id
-        }
-      })
-      if(!profile) throw { name: "NotFound" }
-      
+          userId: id,
+        },
+      });
+      if (!profile) throw { name: "NotFound" };
+
       res.status(200).json({
         id: profile.id,
         name: profile.name,
         image: profile.image,
-        updatedAt: profile.updatedAt
-      })
+        updatedAt: profile.updatedAt,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
 
-module.exports = User
+module.exports = controllerUser;
--- router/user.js
+++ router/user.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const controllerUser = require('../controllers/user')
 
-const User = require('../controllers/user')
 const { authorization } = require('../middlewares/auth')
 
-router.post('/login', User.login)
-router.post('/register', User.register)
-router.put('/profiles', authorization, User.update)
+router.post('/login', controllerUser.login)
+router.post('/register', controllerUser.register)
+router.put('/profiles', authorization, controllerUser.update)
 
 module.exports = router
