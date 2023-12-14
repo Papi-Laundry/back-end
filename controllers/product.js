e6b2b89f50c89684814288d28408b926e16b53a9
nokatotedo <dwikypanduaqri22@gmail.com>
2023-12-14 09:49:40 +0700

    add get all product

--- controllers/product.js
+++ controllers/product.js
@@ -1,73 +1,125 @@
-const Product = require("../models/product")
+const { Product, Category } = require("../models/index")
 
-
-class LaundryProduct{
-    static async createProduct(req,res,next){
-        try {
-            const { laundryId } = req.params
-            const { name, price, description, categoryId, image} = req.body
-
-            const product = await Product.create({
-                name, 
-                price, 
-                description,
-                laundryId, 
-                categoryId, 
-                image
-            })
-
-            res.status(200).json({
-                id : product.id,
-                name : product.name,
-                price : product.price,
-                description : product.description,
-                category : product.categoryId,
-                image : product.image,
-            })
-        } catch (error) {
-            next(error)
+class ProductController {
+  static async getAll(req, res, next) {
+    try {
+      const { laundryId } = req.params
+      if(!Number(laundryId)) throw { name: "InvalidParams" }
+  
+      const products = await Product.findAll({
+        where: {
+          laundryId
+        },
+        include: {
+          model: Category,
+          as: 'category',
+          attributes: ["name"]
         }
+      })
+      res.status(200).json(products)
+    } catch (error) {
+      next(error)
     }
+  }
+  static async create(req,res,next) {
+    try {
+      const { laundryId } = req.params
+      const { name, price, description, categoryId, image} = req.body
 
-    static async updateProduct(req,res,next){
-        try {
-            const { productId } = req.params
+      const product = await Product.create({
+        name, 
+        price, 
+        description,
+        laundryId, 
+        categoryId, 
+        image
+      })
 
-            const { name, price, description, categoryId, image} = req.body
-            const data = await Product.update(req.body, {
-                where : {
-                    id : productId
-                }
-            })
-            const updatedProduct = await Product.findByPk(id)
-            res.status(200).json({updatedProduct})
+      const category = await Category.findByPk(categoryId)
 
-        } catch (error) {
-            next(error)
+      res.status(201).json({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        description: product.description,
+        category: category.name,
+        image: product.image,
+        createdAt: product.createdAt
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async update(req,res,next){
+    try {
+      const { productId } = req.params
+      const { name, price, description, categoryId, image} = req.body
+      if(!Number(productId)) throw { name: "InvalidParams" }
+
+      let product = await Product.findByPk(productId)
+      if(!product) throw { name: "NotFound" }
+
+      await Product.update({
+        name,
+        price,
+        description,
+        categoryId,
+        image
+      }, {
+        where : {
+          id : productId
         }
+      })
+
+      product = await Product.findByPk(productId)
+
+      const category = await Category.findByPk(product.categoryId)
+
+      res.status(200).json({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        description: product.description,
+        category: category.name,
+        image: product.image,
+        updatedAt: product.updatedAt
+      })
+    } catch (error) {
+      next(error)
     }
+  }
 
-    static async deleteProduct(req,res,next){
-        try {
-            const { productId } = req.params
-            if(!productId){
-                throw {name : "NotFoundProductId", productId}
-            }
-            const deleteProduct = await Product.findByPk(id)
-            await Product.destroy({
-                where : {
-                    id : productId
-                }
-            })
-            if(!deleteProduct){
-                throw {name : "NotFoundProduct", productId}
-            }
+  static async delete(req,res,next){
+    try {
+      const { productId } = req.params
+      if(!Number(productId)) {
+        throw { name : "InvalidParams" }
+      }
 
-            res.status(200).json({ message : `ProductId with ${productId} success to delete`})
-        } catch (error) {
-            next(error)
+      const product = await Product.findByPk(productId)
+      if(!product) throw { name: "NotFound" }
+      
+      const category = await Category.findByPk(product.categoryId)
+
+      await Product.destroy({
+        where : {
+          id : productId
         }
+      })
+      
+      res.status(200).json({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        description: product.description,
+        category: category.name,
+        image: product.image,
+      })
+    } catch (error) {
+      next(error)
     }
+  }
 }
 
-module.exports = LaundryProduct
+module.exports = ProductController
